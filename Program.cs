76ac064d014cc4231d8e0697f4b27220b6ef6c1e
Víctor_3.1416 <matemorales2020@gmail.com>
2024-07-29 09:38:29 -0600

    Mod

--- Program.cs
+++ Program.cs
@@ -8,9 +8,30 @@ namespace Laboratorio_1_parte_1
 {
     internal class Program
     {
+        static double precio = 0, total=0;
+        static string nombre = "";
 
         static void Main(string[] args)
         {
+        }
+        static void SolicitarDatos()
+        {
+            Console.Clear();
+            Console.Write("Ingrese el nombre del producto: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Ingrese el precio del producto: Q");
+            precio = double.Parse(nombre);
+        }
+        static double CalcularTotal(double precio)
+        {
+            return total=+precio;
+        }
+        static double Descuento(double total, double porcentaje)
+        {
+            Console.Clear();
+            Console.Write("Ingrese el descuento a aplicar (use unicamente valores enteros sin el simbolo de porcentaje): ");
+            double descuento=
+            return total - total * descuento;
         }
     }
 }
